358b07bc19293aa98524d20285e0d727166bb225
Nicolas Baak <nicolas123alejandro@gmail.com>
2019-09-08 09:21:18 -0500

    Este es el primer intento en C++
    Hice un  minisculo programa en C++ para nada, solo para practicar mi C++, ahora deberia crecer mas para seguir probando GitHub

---
+++ ProyectoC++.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void){
+
+    cout<<"printf no se usa en C"<<endl;
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ProyectoAndroid
 Aprendiendo Android
+
+Vale estas son pruebas para Github espero que esto se actualice en mi github
